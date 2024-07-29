488dfe1ac7c5ab749da43ef4cbb2cebe05e0a2f6
kevinngugi3576 <kevinngugi197@gmail.com>
2024-07-29 15:45:22 +0300

    implement sentry

--- components/TestimonialsGrid.tsx
+++ components/TestimonialsGrid.tsx
@@ -45,9 +45,9 @@ export function TestimonialsGrid() {
           className="w-20 h-20 rounded-full"
         />
         <p className="mt-4  text-sm md:text-xl  font-semibold text-center sm:text-sm p-[10px]">
-          "I've been using PayTeller for over a year now, and I couldn't be
+          I've been using PayTeller for over a year now, and I couldn't be
           happier with the service. The platform is incredibly reliable, and
-          I’ve never encountered any downtime."
+          I’ve never encountered any downtime.
         </p>
       </motion.div>
       <motion.div className="h-full relative z-20 w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
@@ -59,10 +59,10 @@ export function TestimonialsGrid() {
           className="w-20 h-20 rounded-full "
         />
         <p className="mt-4 text-sm md:2xl font-semibold text-center sm:text-sm p-[10px] ">
-          "I was looking for an online banking system that could integrate
+          I was looking for an online banking system that could integrate
           seamlessly with my existing financial tools. PayTeller fit the bill
           perfectly. The integration process was straightforward, and the
-          support team was always available to assist." 
+          support team was always available to assist.
         </p>
       </motion.div>
       <motion.div
@@ -79,7 +79,7 @@ export function TestimonialsGrid() {
         <p className="mt-4 text-sm md:text-xl font-semibold text-center sm:text-sm p-[10px] ">
           One of the standout features of PayTeller is their customer service.
           The team is always friendly, knowledgeable, and quick to respond to
-          any queries or issues I might have." 
+          any queries or issues I might have.
         </p>
       </motion.div>
     </motion.div>
