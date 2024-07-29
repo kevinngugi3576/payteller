7f593d9b82113572421ae85af7138dad54c3f6fc
kevinngugi3576 <kevinngugi197@gmail.com>
2024-07-29 21:42:42 +0300

    implement dependecy array in root components

--- app/(root)/MyBanks/page.tsx
+++ app/(root)/MyBanks/page.tsx
@@ -1,26 +1,24 @@
+
 "use client"
 import Landing from "@/components/Landing";
 import React, { Suspense, useEffect, useState } from "react";
 
 const MyBanks = () => {
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(loading);
+      setLoading(false); // Set loading to false after 1 second
     }, 1000);
 
     return () => clearTimeout(timer); // Clean up the timer
-  }, );
+  }, [loading]);
 
-
-    
-  
-  
-    return (
-      <Suspense fallback={<Landing />}>
-        {loading ? <Landing /> : <Landing />}
-      </Suspense>
-      );
+  return (
+    <Suspense fallback={<Landing />}>
+      {loading ? <Landing /> : <Landing />}
+    </Suspense>
+  );
 };
 
 export default MyBanks;
