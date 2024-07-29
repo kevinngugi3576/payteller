03162c9def5e47a670dcb162e0bc554d3f0724fd
kevinngugi3576 <kevinngugi197@gmail.com>
2024-07-29 16:15:21 +0300

    impplement unescaped characters

--- components/TestimonialsGrid.tsx
+++ components/TestimonialsGrid.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-
 export function TestimonialsGrid() {
   const first = {
     initial: {
@@ -45,9 +44,12 @@ export function TestimonialsGrid() {
           className="w-20 h-20 rounded-full"
         />
         <p className="mt-4  text-sm md:text-xl  font-semibold text-center sm:text-sm p-[10px]">
-          I've been using PayTeller for over a year now, and I couldn't be
-          happier with the service. The platform is incredibly reliable, and
-          I’ve never encountered any downtime.
+          <p className="mt-4 text-sm md:text-xl font-semibold text-center sm:text-sm p-[10px]">
+            &quot;I&apos;ve been using PayTeller for over a year now, and I
+            couldn&apos;t be happier with the service. The platform is
+            incredibly reliable, and I&apos;ve never encountered any
+            downtime.&quot;
+          </p>
         </p>
       </motion.div>
       <motion.div className="h-full relative z-20 w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
@@ -59,10 +61,12 @@ export function TestimonialsGrid() {
           className="w-20 h-20 rounded-full "
         />
         <p className="mt-4 text-sm md:2xl font-semibold text-center sm:text-sm p-[10px] ">
-          I was looking for an online banking system that could integrate
-          seamlessly with my existing financial tools. PayTeller fit the bill
-          perfectly. The integration process was straightforward, and the
-          support team was always available to assist.
+          <p className="mt-4 text-sm md:text-xl font-semibold text-center sm:text-sm p-[10px]">
+            &quot;I was looking for an online banking system that could
+            integrate seamlessly with my existing financial tools. PayTeller fit
+            the bill perfectly. The integration process was straightforward, and
+            the support team was always available to assist.&quot;
+          </p>
         </p>
       </motion.div>
       <motion.div
@@ -76,10 +80,11 @@ export function TestimonialsGrid() {
           width={300}
           className="w-20 h-20 rounded-full"
         />
-        <p className="mt-4 text-sm md:text-xl font-semibold text-center sm:text-sm p-[10px] ">
-          One of the standout features of PayTeller is their customer service.
-          The team is always friendly, knowledgeable, and quick to respond to
-          any queries or issues I might have.
+        <p className="mt-4 text-sm md:text-xl font-semibold text-center sm:text-sm p-[10px] "></p>
+        <p className="mt-4 text-sm md:text-xl font-semibold text-center sm:text-sm p-[10px]">
+          &quot;One of the standout features of PayTeller is their customer
+          service. The team is always friendly, knowledgeable, and quick to
+          respond to any queries or issues I might have.&quot;
         </p>
       </motion.div>
     </motion.div>
